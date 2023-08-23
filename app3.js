90d6ef74bc8f5f56a7ff0a85a08ef7b0a6b752da
Uri Greitser <urigreitser@gmail.com>
2023-08-23 14:55:06 +0300

    Deleting

--- app3.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import bodyParser from 'body-parser';
-import router from './routes/articles.js'
-const app = express();
-app.use(bodyParser());
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
-app.use('/', router);
-app.listen(8080)
